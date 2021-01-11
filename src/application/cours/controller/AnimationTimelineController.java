7b3307795892c8a6a02573c1fdb3b5e4b8ab9ca9
Guillaume QUEFFELEC <queffelec.guillaume@gmail.com>
2021-01-11 17:18:52 +0100

    slide timeline

--- src/application/cours/controller/AnimationTimelineController.java
+++ src/application/cours/controller/AnimationTimelineController.java
@@ -3,16 +3,28 @@ package application.cours.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Circle;
+import javafx.util.Duration;
 
 public class AnimationTimelineController extends Controller {
 
-//	@FXML
-//	private Text text1;
-//	@FXML
-//	private Text text2;
-//	@FXML
-//	private Text text3;
+	@FXML
+	private TextArea text1;
+	@FXML
+	private TextArea text2;
+	@FXML
+	private Circle circle;
+	@FXML
+	private Button play;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -38,4 +50,38 @@ public class AnimationTimelineController extends Controller {
 		}
 	}
 
+	public void timelineCircle(ActionEvent event) {
+
+		Timeline circleAnimation = new Timeline();
+		circleAnimation.setCycleCount(1);
+		KeyFrame k = new KeyFrame(Duration.seconds(2),
+				new KeyValue(circle.translateYProperty(), circle.getTranslateY() + 300));
+		circleAnimation.getKeyFrames().add(k);
+		circleAnimation.play();
+
+	}
+
+	public void timelineCircleBounce(ActionEvent event) {
+
+		Timeline circleAnimation = new Timeline();
+		circleAnimation.setCycleCount(2);
+		KeyFrame k = new KeyFrame(Duration.seconds(2),
+				new KeyValue(circle.translateYProperty(), circle.getTranslateY() - 300));
+		circleAnimation.getKeyFrames().add(k);
+		circleAnimation.setAutoReverse(true);
+		circleAnimation.play();
+
+	}
+
+	public void timelineCircleBounceInfinite(ActionEvent event) {
+
+		Timeline circleAnimation = new Timeline();
+		circleAnimation.setCycleCount(Animation.INDEFINITE);
+		KeyFrame k = new KeyFrame(Duration.seconds(2),
+				new KeyValue(circle.translateYProperty(), circle.getTranslateY() - 300));
+		circleAnimation.getKeyFrames().add(k);
+		circleAnimation.setAutoReverse(true);
+		circleAnimation.play();
+
+	}
 }
